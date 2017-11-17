9bb8537d4e3942b0542ee7fc711ff50d43c9a0fe
zero0r1 <leahdizonth@gmail.com>
2017-11-17 16:44:19 +0800

    AddProduct

---
+++ PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using PartyInvites.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        // GET: Home
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+            return View();
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("thanks", guestResponse);
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        public ViewResult DemoExpression()
+        {
+            Product product = new Product()
+            {
+                ProductId = 1,
+                Name = "Kayak",
+                Description = "A Boat for one person",
+                Category = "Watersports",
+                Price = 275M
+            };
+
+            ViewBag.ProductCount = 1;
+            ViewBag.ExpressShip = true;
+            ViewBag.ApplyDiscount = false;
+            ViewBag.Supplier = null;
+
+            return View(product);
+        }
+    }
+}
