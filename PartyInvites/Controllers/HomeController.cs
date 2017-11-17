c3e22209aaec5bcd83988a3ec5ea892826dba5e8
zero0r1 <leahdizonth@gmail.com>
2017-11-17 17:05:25 +0800

    Commit

--- PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/Controllers/HomeController.cs
@@ -55,5 +55,17 @@ namespace PartyInvites.Controllers
 
             return View(product);
         }
+
+        public ViewResult DemoArray()
+        {
+            Product[] array =
+            {
+                new Product{Name="Kayak",Price=275m},
+                new Product{Name="Lifejacket",Price=48.95m},
+                new Product{Name="Soccer ball",Price=19.50m},
+                new Product{Name="Corner flag",Price=34.95m}
+            };
+            return View(array);
+        }
     }
 }
